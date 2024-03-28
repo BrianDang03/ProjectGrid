440e3402b08d1648e4c5ac06652a01d04246c36d
Brian Dang <pandabrian1@gmail.com>
2024-03-27 20:49:48 -0600

    Add Animation Triggers and Look At
    The unit can now trigger certain animations when moving or staying idle. The unit can now look at the direction that it is going in.

--- Project Grid/Assets/Scripts/Unit.cs
+++ Project Grid/Assets/Scripts/Unit.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Animator unitAnimator;
     [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float rotationSpeed = 5f;
     private Vector3 targetPosition;
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
+
         float stoppingDistance = .01f;
         if (Vector3.Distance(targetPosition, transform.position) > stoppingDistance)
         {
             Vector3 moveDir = (targetPosition - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+            unitAnimator.SetBool(IS_WALKING, true);
             transform.position += moveDir * movementSpeed * Time.deltaTime;
         }
-
-        if (Input.GetMouseButtonDown(0))
+        else
         {
-            Move(MouseWorld.GetPosition());
+            unitAnimator.SetBool(IS_WALKING, false);
         }
     }
 
